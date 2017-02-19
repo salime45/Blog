23d039a480b86fdf9c6daaf5b95fc5fce9e5c093
Ismael Monje <salime45@gmail.com>
2017-02-19 19:22:33 +0000

    Agregado login; Agregada vista de post no publicados

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/'
 
 # default static files settings for PythonAnywhere.
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
